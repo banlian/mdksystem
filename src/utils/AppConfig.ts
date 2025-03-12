614c6f07df94c34ad3762267b924695a46f4f8c8
zzz <zzz@zzz>
2025-03-12 18:47:12 +0800

    rename md

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: 'MDK SYSTEM TOOLKIT',
+  title: 'MDK SYSTEM TOOLKIT',
+  description: 'MDK SYSTEM TOOLKIT',
+  locale: 'zh-CN',
 };
