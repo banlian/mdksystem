65b90029ea30a54ae3f2a0879ddc3718d17e8ff8
zzz <zzz@zzz>
2025-04-17 13:07:41 +0800

    111

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,12 +12,9 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
+          <Link href="">
+            MDK SYSTEM TOOLKIT
           </Link>
-        </li>
-        <li>
-          <Link href="/">Sign in</Link>
         </li>
       </NavbarTwoColumns>
     </Section>
@@ -26,14 +23,13 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            <span className="text-primary-500">MDK SYSTEM TOOLKIT</span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="BUILD YOUR SYSTEM TOOLKIT"
         button={
           <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <Button xl>Download Your Free Theme</Button>
+            <Button xl>MDK SYSTEM TOOLKIT</Button>
           </Link>
         }
       />
