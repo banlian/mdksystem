9cf3115f8f5f744836e47f787f58edef39a7dc71
K <zzz@zzz>
2025-11-21 23:22:04 +0800

    Fix build errors: Remove .babelrc, replace Google Fonts with system fonts

--- src/utils/AppConfig.ts
+++
@@ -1,7 +0,0 @@
-export const AppConfig = {
-  site_name: 'MDK System - Machine Development Kit',
-  title: 'MDK System - Machine Development Kit',
-  description:
-    'Comprehensive software development framework for rapid customization and deployment of industrial automation systems',
-  locale: 'en-US',
-};
